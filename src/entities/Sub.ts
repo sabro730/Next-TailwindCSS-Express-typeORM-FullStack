b1c3875dbe37f2199c0bc1d0cfdd27b9ab38dbf7
golden-lib <tousei.sasaki216@gmail.com>
2021-02-08 15:33:58 +0530

    subs entity for subs table with relations with posts

---
+++ src/entities/Sub.ts
@@ -0,0 +1,37 @@
+import { Entity, Column, Index, ManyToOne, JoinColumn, OneToMany } from 'typeorm'
+
+import Model from './Model'
+import User from './User'
+
+import Post from './Post'
+
+@Entity('subs')
+export default class Sub extends Model {
+  constructor(sub: Partial<Sub>) {
+    super()
+    Object.assign(this, sub)
+  }
+
+  @Index()
+  @Column({ unique: true })
+  name: string
+
+  @Column()
+  title: string
+
+  @Column({ type: 'text', nullable: true })
+  description: string
+
+  @Column({ nullable: true })
+  imageUrn: string
+
+  @Column({ nullable: true })
+  bannerUrn: string
+
+  @ManyToOne(() => User)
+  @JoinColumn({ name: 'username', referencedColumnName: 'username' })
+  user: User
+
+  @OneToMany(() => Post, (post) => post.sub)
+  posts: Post[]
+}
