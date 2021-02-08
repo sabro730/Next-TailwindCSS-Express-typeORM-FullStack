affa1c6d9c96b50ba2c2c11fcb5d1d0f59193e5f
golden-lib <tousei.sasaki216@gmail.com>
2021-02-08 15:31:46 +0530

    abstract model entity

---
+++ src/entities/Model.ts
@@ -0,0 +1,18 @@
+import { PrimaryGeneratedColumn, BaseEntity, CreateDateColumn, UpdateDateColumn } from 'typeorm'
+import { classToPlain, Exclude } from 'class-transformer'
+
+export default abstract class Model extends BaseEntity {
+  @Exclude()
+  @PrimaryGeneratedColumn()
+  id: number
+
+  @CreateDateColumn()
+  createdAt: Date
+
+  @UpdateDateColumn()
+  updatedAt: Date
+
+  toJSON() {
+    return classToPlain(this)
+  }
+}
