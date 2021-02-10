7ebe925762770e2d47515c386e12997ab2d9198b
golden-lib <tousei.sasaki216@gmail.com>
2021-02-11 01:56:35 +0530

    navbar used

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,8 +1,23 @@
-import '../styles/globals.css'
 import { AppProps } from 'next/app'
+import axios from 'axios'
+import { Fragment } from 'react'
+import { useRouter } from 'next/router'
 
+import '../styles/tailwind.css'
+import Navbar from '../components/Navbar'
+
+axios.defaults.baseURL = 'http://localhost:5000/api'
+axios.defaults.withCredentials = true
 function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const { pathname } = useRouter()
+  const authRoutes = ['/register', '/login']
+  const authRoute = authRoutes.includes(pathname)
+  return (
+    <Fragment>
+      {!authRoute && <Navbar />}
+      <Component {...pageProps} />
+    </Fragment>
+  )
 }
 
 export default App
