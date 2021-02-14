77d104cc2206746b1d79bf076fee15718ecb82b1
golden-lib <tousei.sasaki216@gmail.com>
2021-02-14 23:45:43 +0530

    delete oldimages and add new image

--- src/routes/subs.ts
+++ src/routes/subs.ts
@@ -3,6 +3,7 @@ import { isEmpty } from 'class-validator'
 import { getRepository } from 'typeorm'
 import multer, { FileFilterCallback } from 'multer'
 import path from 'path'
+import fs from 'fs'
 
 import User from '../entities/User'
 import Sub from '../entities/Sub'
@@ -107,17 +108,27 @@ const uploadSubImage = async (req: Request, res: Response) => {
     const type = req.body.type
 
     if (type !== 'image' && type !== 'banner') {
+      fs.unlinkSync(req.file.path)
       return res.status(400).json({ error: 'Invalid type' })
     }
 
+    let oldImageUrn: string = ''
     if (type === 'image') {
+      oldImageUrn = sub.imageUrn || ''
       sub.imageUrn = req.file.filename
     } else if (type === 'banner') {
+      oldImageUrn = sub.bannerUrn || ''
       sub.bannerUrn = req.file.filename
     }
     await sub.save()
+
+    if (oldImageUrn !== '') {
+      fs.unlinkSync(`public/images/${oldImageUrn}`)
+    }
+
     return res.json(sub)
   } catch (err) {
+    console.log(err)
     return res.status(500).json({ error: 'Something went wrong' })
   }
 }
