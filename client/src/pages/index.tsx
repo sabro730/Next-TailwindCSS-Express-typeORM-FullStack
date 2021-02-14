5d502becea1d7bcc6f03e8ed17ec46df7fa1539f
golden-lib <tousei.sasaki216@gmail.com>
2021-02-14 21:21:59 +0530

    useSWR for get posts

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -2,37 +2,30 @@ import { useEffect, useState } from 'react'
 import Head from 'next/head'
 import Link from 'next/link'
 import axios from 'axios'
+import useSWR from 'swr'
 
 import { Post } from '../types'
 import PostCard from '../components/PostCard'
 // import { GetServerSideProps } from 'next'
 
 export default function Home() {
-  const [posts, setPosts] = useState<Post[]>([])
-
-  useEffect(() => {
-    const fetchPost = async () => {
-      const responseObject = await axios.get('/posts')
-      setPosts(responseObject.data)
-    }
-    fetchPost()
-  }, [])
+  const { data: posts } = useSWR('/posts')
 
   return (
-    <div className="pt-12">
+    <>
       <Head>
         <title>readit: the front page of the internet</title>
       </Head>
       <div className="container flex pt-4">
         {/* Post feed */}
         <div className="w-160">
-          {posts.map((post) => (
+          {posts?.map((post) => (
             <PostCard post={post} key={post.identifier} />
           ))}
         </div>
         {/* Sidebar */}
       </div>
-    </div>
+    </>
   )
 }
 
