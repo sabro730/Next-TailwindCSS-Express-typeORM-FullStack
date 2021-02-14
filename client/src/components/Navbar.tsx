67220c267960a54037e7d45450d2b97a8f5604d1
golden-lib <tousei.sasaki216@gmail.com>
2021-02-14 14:38:28 +0530

    show logout button if logged in

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,8 +1,24 @@
+import axios from 'axios'
 import Link from 'next/link'
+import { useAuthDispatch, useAuthState } from '../context/auth'
 
 import RedditLogo from '../images/reddit.svg'
 
 const Navbar: React.FC = () => {
+  const { authenticated, loading } = useAuthState()
+
+  const dispatch = useAuthDispatch()
+
+  const logout = async () => {
+    try {
+      await axios.get('/auth/logout')
+      dispatch('LOGOUT')
+      window.location.reload()
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
   return (
     <nav className="fixed inset-x-0 top-0 z-10 flex items-center justify-center h-12 px-5 bg-white">
       {/* Logo and title */}
@@ -27,12 +43,27 @@ const Navbar: React.FC = () => {
       </div>
       {/* Auth buttons */}
       <div className="flex">
-        <Link href="/login">
-          <a className="w-32 py-1 mr-4 leading-5 hollow blue button">log in</a>
-        </Link>
-        <Link href="/register">
-          <a className="w-32 py-1 leading-5 blue button">sign up</a>
-        </Link>
+        {!loading &&
+          (authenticated ? (
+            // Show logout
+            <button
+              className="w-32 py-1 mr-4 leading-5 hollow blue button"
+              onClick={logout}
+            >
+              Logout
+            </button>
+          ) : (
+            <>
+              <Link href="/login">
+                <a className="w-32 py-1 mr-4 leading-5 hollow blue button">
+                  log in
+                </a>
+              </Link>
+              <Link href="/register">
+                <a className="w-32 py-1 leading-5 blue button">sign up</a>
+              </Link>
+            </>
+          ))}
       </div>
     </nav>
   )
