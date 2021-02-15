4ff9acc7a2087e9c683c19c367facbf3b24ad116
golden-lib <tousei.sasaki216@gmail.com>
2021-02-16 00:32:22 +0530

    sidebar used

--- client/src/pages/r/[sub].tsx
+++ client/src/pages/r/[sub].tsx
@@ -9,6 +9,7 @@ import PostCard from '../../components/PostCard'
 import { Sub } from '../../types'
 import { useAuthState } from '../../context/auth'
 import axios from 'axios'
+import Sidebar from '../../components/Sidebar'
 
 const sub = () => {
   // Local state
@@ -134,6 +135,7 @@ const sub = () => {
           {/* Posts & Sidebar */}
           <div className="container flex pt-5">
             <div className="w-160">{postsMarkup}</div>
+            <Sidebar sub={sub} />
           </div>
         </>
       )}
