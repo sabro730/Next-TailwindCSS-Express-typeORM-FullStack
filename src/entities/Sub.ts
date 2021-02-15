95b95b2b32bcb896f0929b3ad24a58717b63adf3
golden-lib <tousei.sasaki216@gmail.com>
2021-02-15 23:07:52 +0530

    unauthenticated error fixed

--- src/entities/Sub.ts
+++ src/entities/Sub.ts
@@ -9,8 +9,8 @@ import {
 
 import Model from './Model'
 import User from './User'
-
 import Post from './Post'
+import { Expose } from 'class-transformer'
 
 @Entity('subs')
 export default class Sub extends Model {
@@ -44,4 +44,18 @@ export default class Sub extends Model {
 
   @OneToMany(() => Post, (post) => post.sub)
   posts: Post[]
+
+  @Expose()
+  get imageUrl(): string {
+    return this.imageUrn
+      ? `${process.env.APP_URL}/images/${this.imageUrn}`
+      : 'https://www.gravatar.com/avatar/00000000000000000000000000000000?d=mp&f=y'
+  }
+
+  @Expose()
+  get bannerUrl(): string | undefined {
+    return this.bannerUrn
+      ? `${process.env.APP_URL}/images/${this.bannerUrn}`
+      : undefined
+  }
 }
