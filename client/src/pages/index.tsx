164c7635595c4ccf1ea6d6926e7bd53a4284c66e
golden-lib <tousei.sasaki216@gmail.com>
2021-02-15 23:52:35 +0530

    sidebar with topSubs

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,15 +1,15 @@
-import { useEffect, useState } from 'react'
 import Head from 'next/head'
 import Link from 'next/link'
-import axios from 'axios'
 import useSWR from 'swr'
 
-import { Post } from '../types'
+import { Sub } from '../types'
 import PostCard from '../components/PostCard'
+import Image from 'next/image'
 // import { GetServerSideProps } from 'next'
 
 export default function Home() {
   const { data: posts } = useSWR('/posts')
+  const { data: topSubs } = useSWR('/misc/top-subs')
 
   return (
     <>
@@ -24,6 +24,40 @@ export default function Home() {
           ))}
         </div>
         {/* Sidebar */}
+        <div className="ml-6 w-80">
+          <div className="bg-white rounded">
+            <div className="p-4 border-b-2">
+              <p className="text-lg font-semibold text-center">
+                Top Communities
+              </p>
+            </div>
+            <div>
+              {topSubs?.map((sub: Sub) => (
+                <div
+                  key={sub.name}
+                  className="flex items-center px-4 py-2 text-xs border-b"
+                >
+                  <div className="mr-2 overflow-hidden rounded-full cursor-pointer">
+                    <Link href={`r/${sub.name}`}>
+                      <Image
+                        src={sub.imageUrl}
+                        alt="Sub"
+                        width={(6 * 16) / 4}
+                        height={(6 * 16) / 4}
+                      />
+                    </Link>
+                  </div>
+                  <Link href={`r/${sub.name}`}>
+                    <a className="font-bold hover:cursor-pointer">
+                      /r/{sub.name}
+                    </a>
+                  </Link>
+                  <p className="ml-auto font-med">{sub.postCount}</p>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
       </div>
     </>
   )
