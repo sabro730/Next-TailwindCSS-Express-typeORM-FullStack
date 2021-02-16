a0a04d7c44f0969d6000ac2b87edde8bca3fdf2b
golden-lib <tousei.sasaki216@gmail.com>
2021-02-17 01:09:00 +0530

    fetching and showing comments + votes functionality for comments

--- client/src/pages/r/[sub]/[identifier]/[slug].tsx
+++ client/src/pages/r/[sub]/[identifier]/[slug].tsx
@@ -9,7 +9,7 @@ import relativeTime from 'dayjs/plugin/relativeTime'
 import classNames from 'classnames'
 
 import Sidebar from '../../../../components/Sidebar'
-import { Post } from '../../../../types'
+import { Post, Comment } from '../../../../types'
 import { useAuthState } from '../../../../context/auth'
 import ActionButton from '../../../../components/ActionButton'
 
@@ -29,20 +29,28 @@ const postPage = () => {
     identifier && slug ? `/posts/${identifier}/${slug}` : null
   )
 
-  const vote = async (value: number) => {
+  const { data: comments, revalidate } = useSWR<Comment[]>(
+    identifier && slug ? `/posts/${identifier}/${slug}/comments` : null
+  )
+  const vote = async (value: number, comment?: Comment) => {
     // If not logged in go to login
     if (!authenticated) router.push('/login')
 
     // If vote is the same reset vote
-    if (value === post.userVote) value = 0
+    if (
+      (!comment && value === post.userVote) ||
+      (comment && comment.userVote === value)
+    )
+      value = 0
 
     try {
-      const responseObject = await axios.post('/misc/vote', {
+      await axios.post('/misc/vote', {
         identifier,
         slug,
+        commentIdentifier: comment?.identifier,
         value,
       })
-      console.log(responseObject.data)
+      revalidate()
     } catch (err) {
       console.log(err)
     }
@@ -77,76 +85,127 @@ const postPage = () => {
         <div className="w-160">
           <div className="bg-white rounded">
             {post && (
-              <div className="flex">
-                {/* Vote Section */}
-                <div className="w-10 py-3 text-center rounded-l">
-                  {/* upvote */}
-                  <div
-                    className="w-6 mx-auto text-gray-400 rounded cursor-pointer hover:bg-gray-300 hover:text-red-500"
-                    onClick={() => vote(1)}
-                  >
-                    <i
-                      className={classNames('icon-arrow-up', {
-                        'text-red-500': post.userVote === 1,
-                      })}
-                    ></i>
+              <>
+                <div className="flex">
+                  {/* Vote Section */}
+                  <div className="flex-shrink-0 w-10 py-2 text-center rounded-l">
+                    {/* upvote */}
+                    <div
+                      className="w-6 mx-auto text-gray-400 rounded cursor-pointer hover:bg-gray-300 hover:text-red-500"
+                      onClick={() => vote(1)}
+                    >
+                      <i
+                        className={classNames('icon-arrow-up', {
+                          'text-red-500': post.userVote === 1,
+                        })}
+                      ></i>
+                    </div>
+                    <p className="text-xs font-bold">{post.voteScore}</p>
+                    {/* downvote */}
+                    <div
+                      className="w-6 mx-auto text-gray-400 rounded cursor-pointer hover:bg-gray-300 hover:text-blue-600"
+                      onClick={() => vote(-1)}
+                    >
+                      <i
+                        className={classNames('icon-arrow-down', {
+                          'text-blue-600': post.userVote === -1,
+                        })}
+                      ></i>
+                    </div>
                   </div>
-                  <p className="text-xs font-bold">{post.voteScore}</p>
-                  {/* downvote */}
-                  <div
-                    className="w-6 mx-auto text-gray-400 rounded cursor-pointer hover:bg-gray-300 hover:text-blue-600"
-                    onClick={() => vote(-1)}
-                  >
-                    <i
-                      className={classNames('icon-arrow-down', {
-                        'text-blue-600': post.userVote === -1,
-                      })}
-                    ></i>
-                  </div>
-                </div>
-                <div className="p-2">
-                  <div className="flex items-center">
-                    <p className="text-xs text-gray-500">
-                      Posted by
-                      <Link href={`/u/${post.username}`}>
-                        <a className="mx-1 hover:underline">
-                          /u/{post.username}
-                        </a>
-                      </Link>
+                  <div className="py-2 pr-2">
+                    <div className="flex items-center">
+                      <p className="text-xs text-gray-500">
+                        Posted by
+                        <Link href={`/u/${post.username}`}>
+                          <a className="mx-1 hover:underline">
+                            /u/{post.username}
+                          </a>
+                        </Link>
+                        <Link href={`${post.url}`}>
+                          <a className="mx-1 hover:underline">
+                            {dayjs(post.createdAt).fromNow()}
+                          </a>
+                        </Link>
+                      </p>
+                    </div>
+                    {/* Post title */}
+                    <h1 className="my-1 text-xl font-medium">{post.title}</h1>
+                    {/* Post body */}
+                    <p className="my-3 text-sm">{post.body}</p>
+                    {/* Actions */}
+                    <div className="flex">
                       <Link href={`${post.url}`}>
-                        <a className="mx-1 hover:underline">
-                          {dayjs(post.createdAt).fromNow()}
+                        <a>
+                          <ActionButton>
+                            <i className="mr-1 fas fa-comment-alt fa-xs"></i>
+                            <span className="font-bold">
+                              {post.commentCount} Comments
+                            </span>
+                          </ActionButton>
                         </a>
                       </Link>
-                    </p>
-                  </div>
-                  {/* Post title */}
-                  <h1 className="my-1 text-xl font-medium">{post.title}</h1>
-                  {/* Post body */}
-                  <p className="my-3 text-sm">{post.body}</p>
-                  {/* Actions */}
-                  <div className="flex">
-                    <Link href={`${post.url}`}>
-                      <a>
-                        <ActionButton>
-                          <i className="mr-1 fas fa-comment-alt fa-xs"></i>
-                          <span className="font-bold">
-                            {post.commentCount} Comments
-                          </span>
-                        </ActionButton>
-                      </a>
-                    </Link>
-                    <ActionButton>
-                      <i className="mr-1 fas fa-share fa-xs"></i>
-                      <span className="font-bold">Share</span>
-                    </ActionButton>
-                    <ActionButton>
-                      <i className="mr-1 fas fa-bookmark fa-xs"></i>
-                      <span className="font-bold">Save</span>
-                    </ActionButton>
+                      <ActionButton>
+                        <i className="mr-1 fas fa-share fa-xs"></i>
+                        <span className="font-bold">Share</span>
+                      </ActionButton>
+                      <ActionButton>
+                        <i className="mr-1 fas fa-bookmark fa-xs"></i>
+                        <span className="font-bold">Save</span>
+                      </ActionButton>
+                    </div>
                   </div>
                 </div>
-              </div>
+                {/* Comment input area */}
+                <hr />
+                {/* Comments feed */}
+                {comments?.map((comment) => (
+                  <div className="flex" key={comment.identifier}>
+                    <div className="flex-shrink-0 w-10 py-2 text-center rounded-l">
+                      {/* upvote */}
+                      <div
+                        className="w-6 mx-auto text-gray-400 rounded cursor-pointer hover:bg-gray-300 hover:text-red-500"
+                        onClick={() => vote(1, comment)}
+                      >
+                        <i
+                          className={classNames('icon-arrow-up', {
+                            'text-red-500': comment.userVote === 1,
+                          })}
+                        ></i>
+                      </div>
+                      <p className="text-xs font-bold">{comment.voteScore}</p>
+                      {/* downvote */}
+                      <div
+                        className="w-6 mx-auto text-gray-400 rounded cursor-pointer hover:bg-gray-300 hover:text-blue-600"
+                        onClick={() => vote(-1, comment)}
+                      >
+                        <i
+                          className={classNames('icon-arrow-down', {
+                            'text-blue-600': comment.userVote === -1,
+                          })}
+                        ></i>
+                      </div>
+                    </div>
+                    <div className="py-2 pr-2">
+                      <p className="mb-1 text-xs leading-none">
+                        <Link href={`/u/${comment.username}`}>
+                          <a className="mr-1 font-bold hover:underline">
+                            {comment.username}
+                          </a>
+                        </Link>
+                        <span className="text-gray-600">
+                          {`
+                            ${comment.voteScore}
+                            points •
+                            ${dayjs(comment.createdAt).fromNow()}
+                          `}
+                        </span>
+                      </p>
+                      <p>{comment.body}</p>
+                    </div>
+                  </div>
+                ))}
+              </>
             )}
           </div>
         </div>
