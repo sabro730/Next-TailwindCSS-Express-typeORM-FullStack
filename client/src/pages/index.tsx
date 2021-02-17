3c05fe946bc840637fe8c4fae8ab70d2046360b2
golden-lib <tousei.sasaki216@gmail.com>
2021-02-17 22:39:41 +0530

    top comunity image fixed

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -37,18 +37,19 @@ export default function Home() {
                   key={sub.name}
                   className="flex items-center px-4 py-2 text-xs border-b"
                 >
-                  <div className="mr-2 overflow-hidden rounded-full cursor-pointer">
-                    <Link href={`r/${sub.name}`}>
+                  <Link href={`r/${sub.name}`}>
+                    <a>
                       <Image
                         src={sub.imageUrl}
                         alt="Sub"
+                        className="rounded-full cursor-pointer"
                         width={(6 * 16) / 4}
                         height={(6 * 16) / 4}
                       />
-                    </Link>
-                  </div>
+                    </a>
+                  </Link>
                   <Link href={`r/${sub.name}`}>
-                    <a className="font-bold hover:cursor-pointer">
+                    <a className="ml-2 font-bold hover:cursor-pointer">
                       /r/{sub.name}
                     </a>
                   </Link>
