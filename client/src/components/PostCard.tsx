404ae5235a9ecf2d80a5fd758f755ab3ab56b5e6
golden-lib <tousei.sasaki216@gmail.com>
2021-02-23 12:23:03 +0530

    sub name and sub iamge only on index page

--- client/src/components/PostCard.tsx
+++ client/src/components/PostCard.tsx
@@ -8,6 +8,7 @@ import { Post } from '../types'
 import ActionButton from './ActionButton'
 import { useAuthState } from '../context/auth'
 import { useRouter } from 'next/router'
+import sub from '../pages/r/[sub]'
 
 dayjs.extend(relativeTime)
 
@@ -29,11 +30,15 @@ const PostCard: React.FC<PostCardProps> = ({
     userVote,
     url,
     username,
+    sub,
   },
   revalidate,
 }) => {
   const { authenticated } = useAuthState()
+
   const router = useRouter()
+
+  const isInSubPage = router.pathname === '/r/[sub]' // /r/[sub]
 
   const vote = async (value: number) => {
     if (!authenticated) router.push('/login')
@@ -83,19 +88,23 @@ const PostCard: React.FC<PostCardProps> = ({
       {/* Post data section */}
       <div className="w-full p-2">
         <div className="flex items-center">
-          <Link href={`/r/${subName}`}>
-            <img
-              src="https://www.gravatar.com/avatar/00000000000000000000000000000000?d=mp&f=y"
-              className="w-6 h-6 mr-1 rounded-full cursor-pointer"
-            />
-          </Link>
-          <Link href={`/r/${subName}`}>
-            <a className="text-xs font-bold cursor-pointer hover:underline">
-              /r/{subName}
-            </a>
-          </Link>
+          {!isInSubPage && (
+            <>
+              <Link href={`/r/${subName}`}>
+                <img
+                  src={sub.imageUrl}
+                  className="w-6 h-6 mr-1 rounded-full cursor-pointer"
+                />
+              </Link>
+              <Link href={`/r/${subName}`}>
+                <a className="text-xs font-bold cursor-pointer hover:underline">
+                  /r/{subName}
+                </a>
+              </Link>
+              <span className="mx-1 text-xs text-gray-500">•</span>
+            </>
+          )}
           <p className="text-xs text-gray-500">
-            <span className="mx-1">•</span>
             Posted by
             <Link href={`/u/${username}`}>
               <a className="mx-1 hover:underline">/u/{username}</a>
